a201d61e1d6c08270ea0f783da0f166bdb2e96b4
Walker <Walker@RAUVOUN-PC>
2023-03-29 17:47:56 +0200

    Add DbContext

--- CreditCardSystem/Controllers/HomeController.cs
+++ CreditCardSystem/Controllers/HomeController.cs
@@ -7,14 +7,20 @@ namespace CreditCardSystem.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly CreditCardSystemContext _dbContext;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, CreditCardSystemContext dbContext)
         {
             _logger = logger;
+            _dbContext = dbContext;
         }
 
         public IActionResult Index()
         {
+            _dbContext.CardType.Add(new CardType { CardTypeId = Guid.NewGuid(), CardTypeName = "VISA" });
+            _dbContext.CardType.Add(new CardType { CardTypeId = Guid.NewGuid(), CardTypeName = "AMEX" });
+            _dbContext.CardType.Add(new CardType { CardTypeId = Guid.NewGuid(), CardTypeName = "MasterCard" });
+            _dbContext.CardType.Add(new CardType { CardTypeId = Guid.NewGuid(), CardTypeName = "Discover" });
             return View();
         }
 
