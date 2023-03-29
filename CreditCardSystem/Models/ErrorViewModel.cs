3d2c815a7689691ca88700ef933ea9cde15e83b8
Walker <Walker@RAUVOUN-PC>
2023-03-29 17:24:38 +0200

    Add project files.

---
+++ CreditCardSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CreditCardSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
